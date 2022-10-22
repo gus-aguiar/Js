deaf58532903edee070ee5297b6475c58864b545
gus-aguiar <gustavopma90@gmail.com>
2022-10-22 12:08:28 -0300

    ultimos requisitos do 10, to bem confuso

--- src/restaurant.js
+++ src/restaurant.js
@@ -98,7 +98,7 @@ let createMenu = (objeto) => {
     fetchMenu: () => objeto,
     consumption: [],
     order: (string) => menuzao.consumption.push(string), 
-    pay: 
+    // pay: () => {for(let index ; index < objeto.food)}
   };
   return menuzao;
 };
@@ -110,5 +110,4 @@ let teste = createMenu({
   drinks: { agua: 3.90, cerveja: 6.90 } });
   // console.log(Object.keys(teste.fetchMenu()));
 
- teste.order('coxinha');
-  console.log(teste.consumption);
+  console.log(Object.values(teste.fetchMenu()));
